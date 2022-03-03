99e8e4dbf96cd806c65e38710ac15862b11ccddc
Kaveri <kaverisg.tce@gmail.com>
2022-03-03 09:45:05 +0530

    BG Flipper FInal code with centered elements

--- BG_Flipper.js
+++ BG_Flipper.js
@@ -1,4 +1,4 @@
-let colorList=["rgba(181, 143, 81)","rgba(146, 181, 81)","rgba(100, 181, 81)","rgba(1, 1, )"];
+let colorList=["rgba(181, 143, 81)","rgba(146, 181, 81)","rgba(100, 181, 81)","rgba(100, 50,10 )"];
 document.getElementById("Click-Me").addEventListener("click",function fun(){
    let index1=Math.floor(Math.random()*10)%4;
    console.log(index1);
